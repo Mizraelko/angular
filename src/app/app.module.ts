3c41113888079442286c8e9f21bd46f2732d58a5
mizraelko <vkoporov@gmail.com>
2022-01-13 22:09:48 +0300

    1 day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {InfoComponent} from "./info/info.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //используемые компоненты
+    AppComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //Необходим для работы ngModel - связывание
   ],
   providers: [],
   bootstrap: [AppComponent]
